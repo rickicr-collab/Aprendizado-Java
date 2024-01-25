e6a596ca0a4814b6d07b69523c19433d1b228d64
rickicr-collab <ricardo_rosendo@ymail.com>
2024-01-25 16:43:50 -0300

    Commit codigo atualizado

---
+++ JavaAula02/src/Classe/Operadores.java
@@ -0,0 +1,20 @@
+package Classe;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		//EXEMPLOS DE VARIAVEIS 
+		// COM ALGUNS EXEMPLOS 
+		int numeroInteiro = 10;
+		long numeroLongo = 	200000l;
+		double numeroDouble = 10d;
+		float numeroFloat = 20f;
+		
+		numeroFloat = (float) numeroDouble;
+		System.out.println("Resto da divisão: "  + (21 % 2));
+		System.out.println(" Numero Double :" + numeroDouble);
+		
+
+	}
+
+}
