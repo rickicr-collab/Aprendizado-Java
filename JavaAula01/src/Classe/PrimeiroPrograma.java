e6ff708de86bde7540de22a7c8ce4cdc91b8633e
rickicr-collab <ricardo_rosendo@ymail.com>
2024-01-24 22:16:50 -0300

    commit aprendizado inicial do java

---
+++ JavaAula01/src/Classe/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package Classe;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		// CRIANDO SEU PRIMEIRO PROGRAMA OLÁ MUNDO
+		System.out.println("Parabens pelo seu primeiro programa ");
+		String palavra = "Olá, Mundo!";
+		System.out.println(palavra);
+
+	}
+
+}
