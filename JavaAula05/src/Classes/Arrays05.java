8aedade66a350ed018e8b73947a2a23cd92a0ff9
rickicr-collab <ricardo_rosendo@ymail.com>
2024-01-30 12:02:26 -0300

    Commit classe nova sobre arrays com informaçoes novas e atualizadas

---
+++ JavaAula05/src/Classes/Arrays05.java
@@ -0,0 +1,39 @@
+package Classes;
+
+public class Arrays05 {
+	/*
+	 * CONTINUANDO O ESTUDO DE ARRAYS MULTIDIMENSIONAIS 
+	 * OBS 1: SO É NESSECÁRIO DECLARAR O TAMANHO DO PRIMEIRO ARRAY DELCARADO 
+	 * DO ARRAYS MULTIDMENSIONAL NÃO É NECESSARIO DECLARA UM PRE VALOR DE ESPACOS 
+	 */
+
+	public static void main(String[] args) {
+		/* 
+		 * TRABALHANDO COM ARRAYS MULTIDIMENSIONAIS 
+		 * FAZENDO REFERENCIA A ARRAYS DE DIFERENTES 
+		 * TAMANHOS PARA CADA POSIÇÃO ESPECIFICA DO PRIMEIRO ARRAY
+		 */
+		int[][] dias = new int[3][];
+		/*
+		 * POSIÇÃO 0: ADICIONANDO UM ARRAY INSTANCIANDO ADICIONANDO O TAMANHO DO MESMO DENTRO DO COLCHETES.
+		 * POSIÇÃO 1: INSTANCIANDO UM ARRAY COM O NUMEROS DE ELEMENTOS DENTRO DELE.
+		 * 
+		 */
+		dias[0] = new int[2];
+		dias[1] = new int[]{1, 2, 3};
+		dias[2] = new int[4];
+		
+		//UTILIZANDO FOR PARA VISUALIZAR DADOS DOS ARRAYS MULTIDIMENSIONAIS 
+		System.out.println("----- Imprimindo dados em Tela----- ");
+		for(int[] arr: dias) {
+			System.out.println("Endereços : " + arr);
+			for(int num: arr) {
+				System.out.println("Array Valores: " + num);
+				
+			}
+		}
+		
+
+	}
+
+}
