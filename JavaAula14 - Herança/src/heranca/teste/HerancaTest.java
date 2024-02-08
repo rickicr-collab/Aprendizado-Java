672e6de4d3058c0dd60e7c7ac4f0ad29df5ee84d
rickicr-collab <ricardo_rosendo@ymail.com>
2024-02-08 16:22:49 -0300

    Commit Codigo atualizado

--- JavaAula14 - Herança/src/heranca/teste/HerancaTest.java
+++ JavaAula14 - Herança/src/heranca/teste/HerancaTest.java
@@ -7,19 +7,19 @@ import heranca.classes.Pessoa;
 public class HerancaTest {
 
 	public static void main(String[] args) {
-		//Pessoa pessoa = new Pessoa("Ricardo");
+		Pessoa pessoa = new Pessoa("Ricardo");
 		Endereco end = new Endereco();
-		//pessoa.setCpf("051.132.574-60");
-		//pessoa.setRg("963287-6");
+		pessoa.setCpf("051.132.574-60");
+		pessoa.setRg("963287-6");
 		end.setRua("Rua Primeira");
 		end.setBairro("Rio de Janeito");
-		//pessoa.setEndereco(end);
-		//pessoa.imprimir();
+		pessoa.setEndereco(end);
+		pessoa.imprimir();
 		System.out.println("------------------------------------");
 		Funcionario f = new Funcionario("Joana", 15000);
 		f.setCpf("055.333.222-10");
 		f.setEndereco(end);
-		//f.imprimir();
+		f.imprimir();
 		
 		
 
