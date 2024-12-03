687715ce3e491507a36f6ceb4476356c1757ba4b
Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>
2024-12-03 11:18:44 -0300

    pacote da classe equals colecoes atualizado
    Signed-off-by: Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>

---
+++ JavaAula31 - Colecoes/src/colecoes/colecoesatualizado/equals/ClassEqualsMain.java
@@ -0,0 +1,19 @@
+package colecoesatualizado.equals;
+
+public class ClassEqualsMain {
+	public static void main(String[] args) {
+		titulo();
+		Smartphone celular1 = new Smartphone("14BCQ", "Samsung");
+		Smartphone celular2 = celular1;
+		
+		System.out.println("Resultado: " + celular1.equals(celular2));
+		
+	}
+	
+	public static void titulo() {
+		System.out.println("-----------------------------------");
+		System.out.println("      Coleções Classe Equals       ");
+		System.out.println("-----------------------------------");
+	}
+
+}
---
+++ JavaAula31 - Colecoes/src/colecoes/colecoesatualizado/equals/Smartphone.java
@@ -0,0 +1,38 @@
+package colecoesatualizado.equals;
+
+public class Smartphone {
+	private String serialNumber;
+	private String marca;
+	
+	
+	public Smartphone(String serialNumber, String marca) {
+		super();
+		this.serialNumber = serialNumber;
+		this.marca = marca;
+	}
+
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+	
+	
+
+}
